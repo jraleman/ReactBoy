98b2068a63ddd54696efe7d2b800f1adbe3f9d8f
jraleman <jraleman@bendiburg.com>
2018-11-02 18:21:53 -0700

    Add dummy cpu component

---
+++ src/components/CPU/CPU.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CPU extends React.Component {
+  constructor (props) {
+    super(props);
+    return ;
+  }
+  render () {
+    return (
+      <React.Fragment>
+        <p>{ "CPU" }</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default CPU;
